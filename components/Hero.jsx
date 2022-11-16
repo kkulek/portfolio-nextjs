86f0a6c20f8d07384e9b17cbb1e94d38cff293f9
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-16 21:53:01 +0100

    changed opacity in the background

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,10 +4,10 @@ export function Hero() {
     return (
         <section className="h-screen">
             <div className="flex h-full items-center justify-center relative">
-                <div className="absolute bottom-1/2 w-36 h-36 sm:w-52 sm:h-52 rounded-full bg-cyan-200 mix-blend-multiply filter blur-3xl opacity-50 animate-blob"></div>
-                <div className="absolute left-1/2 w-36 h-36 sm:w-52 sm:h-52 rounded-full bg-purple-300 mix-blend-multiply filter blur-3xl opacity-50 animate-blob animation-delay-1000"></div>
-                <div className="absolute top-1/2 w-36 h-36 sm:w-52 sm:h-52 rounded-full bg-yellow-200 mix-blend-multiply filter blur-3xl opacity-50 animate-blob animation-delay-2000"></div>
-                <div className="absolute right-1/2 w-36 sm:w-52 sm:h-52 rounded-full bg-pink-300 mix-blend-multiply filter blur-3xl opacity-50 animate-blob animation-delay-3000"></div>
+                <div className="absolute bottom-1/2 w-36 h-36 sm:w-52 sm:h-52 rounded-full bg-cyan-200 mix-blend-multiply filter blur-3xl opacity-90 animate-blob"></div>
+                <div className="absolute left-1/2 w-36 h-36 sm:w-52 sm:h-52 rounded-full bg-purple-300 mix-blend-multiply filter blur-3xl opacity-90 animate-blob animation-delay-1000"></div>
+                <div className="absolute top-1/2 w-36 h-36 sm:w-52 sm:h-52 rounded-full bg-yellow-200 mix-blend-multiply filter blur-3xl opacity-90 animate-blob animation-delay-2000"></div>
+                <div className="absolute right-1/2 w-36 sm:w-52 sm:h-52 rounded-full bg-pink-300 mix-blend-multiply filter blur-3xl opacity-90 animate-blob animation-delay-3000"></div>
                 <div className="z-10 -mt-20">
                     <h1 className="text-5xl font-bold tracking-wider">Frontend developer</h1>
                     <p className="text-3xl font-light tracking-wider">with digital marketing background</p>
