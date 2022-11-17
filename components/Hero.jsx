df6d8cba9bb64fcd8344c686c7902aa4df29a2e9
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-17 21:27:45 +0100

    added navlinks (hyperlinks)

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import {Button} from "./Button";
 
 export function Hero() {
     return (
-        <section className="h-screen">
+        <section id="home" className="h-screen">
             <div className="flex h-full items-center justify-center relative">
                 <div className="absolute bottom-1/2 w-36 h-36 sm:w-52 sm:h-52 rounded-full bg-cyan-200 mix-blend-multiply filter blur-3xl opacity-90 animate-blob"></div>
                 <div className="absolute left-1/2 w-36 h-36 sm:w-52 sm:h-52 rounded-full bg-purple-300 mix-blend-multiply filter blur-3xl opacity-90 animate-blob animation-delay-1000"></div>
@@ -12,8 +13,8 @@ export function Hero() {
                     <h1 className="text-5xl font-bold tracking-wider">Frontend developer</h1>
                     <p className="text-3xl font-light tracking-wider">with digital marketing background</p>
                     <div className="space-x-4 mt-6">
-                        <Link href="/" className="px-4 py-2 bg-slate-200 rounded">Projects</Link>
-                        <Link href="/" className="px-4 py-2 bg-slate-50 rounded">Github</Link>
+                        <Button link="#projects">Projects</Button>
+                        <Button link="https://github.com/kkulek" github>Github</Button>
                     </div>
                 </div>
                 <div className="absolute z-10 w-full h-52 xl:h-56 bg-bottom-layer -bottom-20 bg-no-repeat bg-cover bg-bottom"></div>
