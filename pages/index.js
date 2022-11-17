85386b32c480567326a848ac8a6bcd0e73ce5c9c
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-17 16:11:32 +0100

    added Navigation component

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import {Hero} from "../components/Hero";
 import {Skills} from "../components/Skills";
 import {Projects} from "../components/Projects";
 import {Contact} from "../components/Contact";
+import {Navigation} from "../components/Navigation";
 
 export default function Home() {
     return (
         <>
+            <Navigation />
             <main>
                 <Hero/>
                 <Skills/>
