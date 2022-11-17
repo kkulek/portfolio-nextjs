568f57a575072be0b6e0a800d613a19ada52cff3
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-17 01:23:51 +0100

    added dummy data for projects component

--- data.js
+++ data.js
@@ -55,3 +55,86 @@ export const skills = [
         ],
     }
 ];
+
+export const projects = [
+    {
+        name: "Devjobs board",
+        img: "project-devjobs.webp",
+        short: "SPA with routing, dark mode & search functionality: React & Tailwindcss.",
+        stackArr: [
+            "HTML", "React", "Tailwindcss",
+        ],
+        featuresArr: [
+            "Dark mode", "User search", "Routing"
+        ]
+    },
+    {
+        name: "Countries search",
+        img: "project-countries.webp",
+        short: "SPA using: React, Tailwindcss, API.",
+        stackArr: [
+            "HTML", "React", "Tailwindcss",
+        ],
+        featuresArr: [
+            "Dark mode", "Countries search", "Fetch API", "Routing"
+        ]
+    },
+    {
+        name: "Password Generator",
+        img: "project-password-generator.webp",
+        short: "Practical app using: HTML, Tailwindcss & JS.",
+        stackArr: [
+            "HTML", "JS", "Tailwindcss",
+        ],
+        featuresArr: [
+            "Dark mode", "Generating password"
+        ]
+    },
+    {
+        name: "Scoot website",
+        img: "projects-scoot-lp.webp",
+        short: "Multi page layout using: HTML, SASS, JS.",
+        stackArr: [
+            "HTML", "SASS", "JS",
+        ],
+        featuresArr: [
+            "Dark mode", "Generating password"
+        ]
+    },
+    // {
+    //     name: "Github user search",
+    //     img: "",
+    //     short: "Search app using: React, styled-components, fetch API.",
+    //     stack: "",
+    // },
+    // {
+    //     name: "Advice generator",
+    //     img: "",
+    //     short: "Simple app using: React, styled-components, fetch API.",
+    //     stack: "",
+    // },
+    // {
+    //     name: "CRUD Kanban",
+    //     img: "",
+    //     short: "Kanban board using: React, Tailwindcss, fetch API, beutiful-dnd, uuidv4 and Firestore.",
+    //     stack: "",
+    // },
+    // {
+    //     name: "Easybank Single Page",
+    //     img: "",
+    //     short: "HTML, SASS & JS.",
+    //     stack: "",
+    // },
+    // {
+    //     name: "Meet Single Page",
+    //     img: "",
+    //     short: "HTML & CSS",
+    //     stack: "",
+    // },
+    // {
+    //     name: "Rating component",
+    //     img: "",
+    //     short: "My first JS component.",
+    //     stack: "",
+    // },
+]
