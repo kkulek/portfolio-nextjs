2dd0a2196e7f3062bdc209c2c7f0a5f25e2922de
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-17 13:33:28 +0100

    added Contact component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import {Hero} from "../components/Hero";
 import {Skills} from "../components/Skills";
 import {Projects} from "../components/Projects";
+import {Contact} from "../components/Contact";
 
 export default function Home() {
     return (
-        <main>
-            <Hero/>
-            <Skills/>
-            <Projects />
-        </main>
+        <>
+            <main>
+                <Hero/>
+                <Skills/>
+                <Projects/>
+            </main>
+            <Contact/>
+        </>
     )
 }
