83567603b83bf0cab4efb11e80dc3af6bc2d17d5
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-18 20:45:22 +0100

    temp solution for #position during scrolling

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 
 export function Projects() {
     return (
-        <section className="px-10 sm:px-20 py-32">
+        <section className="px-10 sm:px-20 py-32" id="projects">
             <div className="mx-auto my-48 max-w-xl lg:max-w-[1400px]">
-                <h2 id="projects" className="text-6xl md:text-7xl font-bold max-w-[1200px] mx-auto">Projects:</h2>
+                <h2 className="text-6xl md:text-7xl font-bold max-w-[1200px] mx-auto">Projects:</h2>
                 <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-12 my-24">
                     {projects.map((item, index) => {
                         const basePath = '/img/projects/';
@@ -27,17 +27,19 @@ export function Projects() {
                                             <h3 className="transition-all duration-900 font-bold tracking-wider text-3xl mb-4 cursor-pointer">{item.name}
                                             </h3>
                                         </Link>
-                                        <ul className="space-x-2">
-                                            <hX>Stack:</hX>
-                                            {item.stackArr.map((stack, index) =>
-                                                (
-                                                    <li key={index}
-                                                        className="inline-block font-light px-1 py-1 bg-slate-100 rounded text-sm mb-4">
-                                                        {stack}
-                                                    </li>
-                                                )
-                                            )}
-                                        </ul>
+                                        <div className="flex gap-2">
+                                            <span>Stack:</span>
+                                            <ul className="space-x-2">
+                                                {item.stackArr.map((stack, index) =>
+                                                    (
+                                                        <li key={index}
+                                                            className="inline-block font-light px-1 py-1 bg-slate-100 rounded text-sm mb-4">
+                                                            {stack}
+                                                        </li>
+                                                    )
+                                                )}
+                                            </ul>
+                                        </div>
                                         <p className="font-thin text-lg mb-8">{item.short}</p>
                                         <div className="space-x-4 mt-6">
                                             <Button link={item.link.live} target="_blank">Preview</Button>
