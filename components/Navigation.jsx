2d687e5c23ff08352f1709b8e322879d7932df00
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-18 18:30:55 +0100

    updated component (fully responsive)

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,53 +1,51 @@
 import Link from "next/link";
+import {useState} from "react";
 
 export function Navigation() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isScrollable, setIsScrollable] = useState(true);
+
+    const handleHamburger = () => {
+        setIsOpen(!isOpen)
+        isScrollable ? document.body.style.overflow = "hidden" : document.body.style.overflow = "auto"
+        setIsScrollable(!isScrollable)
+    }
+
+    const navLinks = [
+        {name: "Home", link: "#home"},
+        {name: "Skills", link: "#skills"},
+        {name: "Projects", link: "#projects"},
+        {name: "Contact", link: "#contact"},
+    ];
+
     return (
         <div
-            className="fixed w-full px-10 sm:px-20 z-20 ease-in duration-300 bg-white shadow-md bg-opacity-90 hover:bg-opacity-100  transition-all duration-400">
+            className="fixed w-full px-10 sm:px-20 z-20 ease-in duration-300 bg-white shadow-md bg-opacity-90 hover:bg-opacity-100 transition-all duration-400">
             <div className="max-w-[1200px] mx-auto">
                 <nav className="flex justify-between items-center py-2">
-                    <Link href="#home"><img src="/img/logo/logo-dark.webp" alt="logo"
-                                            className="w-[117px] h-[31px] w-full h-fit ml-[4px]"></img></Link>
-                    <ul className="hidden md:flex px-6 gap-10 text-prm-dark text-sm font-bold tracking-widest">
-                        <li>
-                            <Link href="#home" className="hover:text-palette-0 transition-all duration-400">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="#skills"
-                                  className="hover:text-palette-0 transition-all duration-400">Skills</Link>
-                        </li>
-                        <li>
-                            <Link href="#projects"
-                                  className="hover:text-palette-0 transition-all duration-400">Projects</Link>
-                        </li>
-                        <li>
-                            <Link href="#contact"
-                                  className="hover:text-palette-0 transition-all duration-400">Contact</Link>
-                        </li>
+                    <Link href="#home">
+                        <img src="/img/logo/logo-dark.webp" alt="logo"
+                             className="w-[117px] h-[31px] w-full h-fit ml-[4px]">
+                        </img>
+                    </Link>
+                    <div className="absolute right-10 cursor-pointer md:hidden" onClick={handleHamburger}>
+                        {isOpen
+                            ? <span className="block w-6 h-3 bg-blue-600 mb-1 rounded"><ion-icon name="close"></ion-icon></span>
+                            : <span className="block w-6 h-4 bg-gray-600 mb-1 rounded"><ion-icon name="menu"></ion-icon></span>
+                        }
+                    </div>
+                    <ul className={`bg-white md:bg-transparent flex flex-col items-center md:flex-row absolute md:static 
+                    top-11 -right-full w-full md:w-auto h-screen md:h-auto pt-10 md:pt-0 gap-12 text-prm-dark text-sm font-bold tracking-widest
+                    transition-all duration-200 ease-in opacity-0 md:opacity-100 
+                    ${isOpen? 'right-0 opacity-100 overflow-hidden' : '-right-full opacity-0'}`}
+                    >
+                        {navLinks.map((item, index) =>
+                            <li key={index}>
+                                <Link href={item.link}
+                                      className="hover:text-palette-0 transition-all duration-400 px-8 py-2 md:px-0 md:py-0" onClick={handleHamburger}>{item.name}</Link>
+                            </li>
+                        )}
                     </ul>
-                    <button className="block md:hidden cursor-pointer group">
-                        <span className="block w-6 h-1 bg-gray-600 mb-1 rounded"></span>
-                        <span className="block w-6 h-1 bg-gray-600 mb-1 rounded"></span>
-                        <span className="block w-6 h-1 bg-gray-600 mb-1 rounded"></span>
-                        <div className=
-                                 "absolute md:hidden top-0 -right-full h-fit w-3/5 border opacity-0 bg-white
-                                 group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
-                            <ul>
-                                <li className="py-4 px-6 w-full hover:bg-slate-200 hover:text-palette-0 hover:font-bold transition-all duration-400">
-                                    <Link href="#home">Home</Link>
-                                </li>
-                                <li className="py-4 px-6 w-full hover:bg-slate-200 hover:text-palette-0 hover:font-bold transition-all duration-400">
-                                    <Link href="#skills">Skills</Link>
-                                </li>
-                                <li className="py-4 px-6 w-full hover:bg-slate-200 hover:text-palette-0 hover:font-bold transition-all duration-400">
-                                    <Link href="#projects">Projects</Link>
-                                </li>
-                                <li className="py-4 px-6 w-full hover:bg-slate-200 hover:text-palette-0 hover:font-bold transition-all duration-400">
-                                    <Link href="#contact">Contact</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </button>
                 </nav>
             </div>
         </div>
